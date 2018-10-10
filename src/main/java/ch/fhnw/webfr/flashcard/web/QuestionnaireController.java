0810bd09f550c831d73c2d96d36783f59598578a
Patrick Fischer <patrick.fischer.privat@gmail.com>
2018-10-10 17:09:36 +0200

    initial

---
+++ src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
@@ -0,0 +1,43 @@
+package ch.fhnw.webfr.flashcard.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ch.fhnw.webfr.flashcard.domain.Questionnaire;
+import ch.fhnw.webfr.flashcard.persistence.QuestionnaireRepository;
+
+@Controller
+@RequestMapping("/questionnaires")
+public class QuestionnaireController {
+	
+	@Autowired
+	private QuestionnaireRepository questionnaireRepository;
+
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String findAll(Model model) {
+		
+		
+		List<Questionnaire> questionnaires = questionnaireRepository.findAll();
+		
+		model.addAttribute("questionnaires", questionnaires);
+		
+		return "questionnaires/list";
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public String findById(@PathVariable String id, Model model) {
+		
+		Optional<Questionnaire> questionnaire = questionnaireRepository.findById(id);
+		model.addAttribute("questionnaire", questionnaire.get());
+		
+		return "questionnaires/show";
+	}
+}
