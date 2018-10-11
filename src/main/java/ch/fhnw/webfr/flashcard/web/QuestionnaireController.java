e317f1b44b181419dde4dc0185dab3bd163e6823
Patrick Fischer <patrick.fischer.privat@gmail.com>
2018-10-11 21:54:12 +0200

    Finish UB4

--- src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
+++ src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,12 +14,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import ch.fhnw.webfr.flashcard.domain.Questionnaire;
 import ch.fhnw.webfr.flashcard.persistence.QuestionnaireRepository;
 
+import javax.jws.WebParam;
+
 @Controller
 @RequestMapping("/questionnaires")
 public class QuestionnaireController {
 	
 	@Autowired
 	private QuestionnaireRepository questionnaireRepository;
+
+	@RequestMapping(params = "form", method = RequestMethod.GET)
+	public String createForm(Model model) {
+		model.addAttribute("questionnaire", new Questionnaire());
+		return "questionnaires/create";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String create(Questionnaire questionnaire) {
+		questionnaireRepository.save(questionnaire);
+		return "redirect:/questionnaires";
+	}
 
 	
 	@RequestMapping(method = RequestMethod.GET)
